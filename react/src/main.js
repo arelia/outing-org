d9061fccd42d438f256f08e84887bb7e3a42b4d4
arelia <arelia.jones@gmail.com>
2017-01-01 00:13:10 -0600

    Add React

--- react/src/main.js
+++ react/src/main.js
@@ -1,1 +1,10 @@
-console.log('Where do we want to go today?');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
